12cb090c2cff88255de5b215f53344df8e1f1736
anderssb <anderssb@gmail.com>
2014-11-25 19:32:41 +0100

    Cleaning of the files in the repository.

--- vid_stabilize.m
+++ vid_stabilize.m
@@ -20,7 +20,7 @@ sigma_b = 0.0002;           % - translation, b1/b2
 sigma_obs_z = 1;            % - rotation, a2/a3
 sigma_obs_r = 1;            % - zoom, a1/a4
 sigma_obs_b = 1;            % - translation, b1/b2
-tic
+
 % Load the video
 [vid_org,vid] = load_videoreader(current_vid,start_pos,num_frames);
 
@@ -35,7 +35,7 @@ S_lit = litvin_method(T,sigma_z,sigma_r,sigma_b,sigma_obs_z,sigma_obs_r,sigma_ob
 vid_lit = warp(vid_org,S_lit,crop_border);
 vid_mat = warp(vid_org,S_mat,crop_border);
 vid_crop = crop(vid_org,crop_border);
-toc
+
 % Compare both videos to the original
 play_video(vid_crop,vid_lit,num_play,'compare');
 play_video(vid_crop,vid_mat,num_play,'compare');
